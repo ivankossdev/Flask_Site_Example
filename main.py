68bf943a04eb5d25ec2c41d597b8fe8ea1f95d2b
ivankossdev <ivan.koss@yandex.ru>
2022-12-01 12:57:29 +0300

    Пример сайта

---
+++ main.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/price')
+def price():
+    return render_template('price.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+if __name__ == '__main__':
+    app.run()
